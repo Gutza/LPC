d4cb54619b295763eb3995dde0011f98698ee339
Bogdan Stancescu <bogdan@moongate.ro>
2011-09-19 18:49:00 +0200

    Support for pre-preparation

--- include/classes/LPC_HTML_widget.php
+++ include/classes/LPC_HTML_widget.php
@@ -4,6 +4,7 @@ abstract class LPC_HTML_widget extends LPC_HTML_node
 {
 
 	protected $unconditional_preparation=false;
+	private $pre_prepared=false;
 
 	abstract public function prepare();
 
@@ -12,6 +13,14 @@ abstract class LPC_HTML_widget extends LPC_HTML_node
 		if (!$this->content || $this->unconditional_preparation)
 			$this->prepare();
 		return parent::render();
+	}
+
+	function pre_prepare()
+	{
+		if ($this->pre_prepared)
+			return;
+		$this->prepare();
+		$this->pre_prepared=true;
 	}
 
 }
