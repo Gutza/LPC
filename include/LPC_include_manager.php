81c8b810ca6b2b1a96bdcc32b63eaec307c44804
Bogdan Stancescu <bogdan@moongate.ro>
2011-12-06 22:37:20 +0200

    Legacy support for $LPC_extra_class_dirs; might also be needed for scaffolding, we'll see

--- include/LPC_include_manager.php
+++ include/LPC_include_manager.php
@@ -3,11 +3,20 @@
 function LPC_autoloader($class_name)
 {
 	$fname=LPC_classes.'/'.$class_name.'.php';
-	if (!file_exists($fname))
-		return false;
+	if (file_exists($fname)) {
+		include $fname;
+		return true;
+	}
 
-	include $fname;
-	return true;
+	global $LPC_extra_class_dirs;
+	foreach($LPC_extra_class_dirs as $dir) {
+		$fname=$dir.'/'.$class_name.'.php';
+		if (file_exists($fname)) {
+			include $fname;
+			return true;
+		}
+	}
+	return false;
 }
 
 spl_autoload_register('LPC_autoloader',false);
