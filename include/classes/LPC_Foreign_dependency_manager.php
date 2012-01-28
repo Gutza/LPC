892c2effe1724f1aadaa4322dfd150d9c56f9480
Bogdan Stancescu <bogdan@moongate.ro>
2012-01-28 02:55:05 +0200

    Renamed for consistency

--- include/classes/LPC_Foreign_dependency_manager.php
+++ include/classes/LPC_Foreign_dependency_manager.php
@@ -5,9 +5,9 @@
  * @author Bogdan Stancescu <bogdan@moongate.ro>
  * @copyright Copyright (c) November 2009, Bogdan Stancescu
  * @license http://www.gnu.org/licenses/gpl.html GNU Public License v3 or later
- * @version $Id: LPC_foreign_dependency_manager.php,v 1.3 2011/01/12 21:35:24 bogdan Exp $
+ * @version $Id$
  */
-class LPC_foreign_dependency_manager
+class LPC_Foreign_dependency_manager
 {
 	static private $dependencies=array();
 
@@ -18,6 +18,7 @@ class LPC_foreign_dependency_manager
 		foreach($depData as $k=>$v)
 			if (!isset($v['dbKey']))
 				throw new RuntimeException("Foreign dependencies MUST define the database key (key 'dbKey' in dependency '$k')");
+
 		if (!isset(self::$dependencies[$class]))
 			self::$dependencies[$class]=$depData;
 		else
