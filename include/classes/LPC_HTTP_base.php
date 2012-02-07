3914168a753c4c461e6911ef297349a554bbb8dc
Bogdan Stancescu <bogdan@moongate.ro>
2012-02-07 18:38:29 +0200

    Added a couple of classes to help with file and image management

---
+++ include/classes/LPC_HTTP_base.php
@@ -0,0 +1,135 @@
+<?php
+/**
+* The generic LPC class for serving HTTP content.
+* @author Bogdan Stancescu <bogdan@moongate.ro>
+* @copyright Copyright (c) February 2012, Bogdan Stancescu
+* @license http://www.gnu.org/licenses/gpl.html GNU Public License v3 or later
+*/
+class LPC_HTTP_base
+{
+	/**
+	* The MIME type associated with this file.
+	*/
+	public $mimeType="binary/octet-stream";
+
+	/**
+	* The name of this file, when it is downloaded.
+	*/
+	public $fileName="";
+
+	/**
+	* The date at which the file was last modified.
+	*/
+	public $date=0;
+
+	/**
+	* Whether to serve this as an attachment
+	* (by default it's served inline).
+	*/
+	public $asAttachment=false;
+
+	/**
+	* The actual content of this file.
+	*/
+	public $content="";
+
+	/**
+	* If the {@link $date} variable is not set and this is set,
+	* the file will be cached for at most these many seconds
+	* (Cache-Control: max-age=$cacheMaxAge)
+	*/
+	public $cacheMaxAge=0;
+
+	/**
+	* Loads all relevant data from a LPC_Object descendant.
+	*/
+	public function fromObject($object,$fileName)
+	{
+		if (!is_object($object) || !$object instanceof LPC_Object)
+			throw new RuntimeException("Expecting a LPC_Object instance!");
+
+		if (!$object->isValidFile($fileName))
+			throw new RuntimeException("File identifier is invalid (".$fileName.")!");
+
+		$this->content=$object->getAttr($object->dataStructure['files'][$fileName]['content']);
+		if (isset($object->dataStructure['files'][$fileName]['mime']))
+			$this->mimeType=$object->getAttr($object->dataStructure['files'][$fileName]['mime']);
+		if (isset($object->dataStructure['files'][$fileName]['name']))
+			$this->fileName=$object->getAttr($object->dataStructure['files'][$fileName]['name']);
+		if (isset($object->dataStructure['files'][$fileName]['date']))
+			$this->date=$object->getAttr($object->dataStructure['files'][$fileName]['date']);
+	}
+
+	/**
+	* Serves the file to the client.
+	*/
+	public function show()
+	{
+		$this->headerContentType();
+		$this->headerContentDisposition();
+		$this->headerContentLength();
+		$this->cacheControl();
+		$this->otherHeaders();
+		$this->servePayload();
+	}
+
+	/**
+	* Sends the Content-type header.
+	*/
+	protected function headerContentType()
+	{
+		header("Content-type: ".$this->mimeType);
+	}
+
+	/**
+	* Sends the Content-disposition header
+	*/
+	protected function headerContentDisposition()
+	{
+		if (strlen($this->fileName))
+			$fileName="; filename=\"".addslashes($this->fileName)."\"";
+		else
+			$fileName="";
+		if ($this->asAttachment)
+			$disposition="attachment";
+		else
+			$disposition="inline";
+		header("Content-disposition: ".$disposition.$fileName);
+	}
+
+	/**
+	* Sends the Content-length header
+	*/
+	protected function headerContentLength()
+	{
+		header("Content-length: ".strlen($this->content));
+	}
+
+	/**
+	* Override this if you need to send other headers.
+	*/
+	protected function otherHeaders()
+	{
+	}
+
+	/**
+	* Cache control calls to LPC_Browser_cache
+	*/
+	protected function cacheControl()
+	{
+		if ($this->date)
+			return LPC_Browser_cache::dateCache($this->date);
+
+		if ($this->cacheMaxAge)
+			return LPC_Browser_cache::ageCache($this->cacheMaxAge);
+	}
+
+	/**
+	* Serves the actual content. This MUST always exit.
+	*/
+	protected function servePayload()
+	{
+		echo $this->content;
+		exit;
+	}
+}
---
+++ include/classes/LPC_HTTP_image.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+* The generic LPC image class
+* @author Bogdan Stancescu <bogdan@moongate.ro>
+* @copyright Copyright (c) January 2012, Bogdan Stancescu
+* @license http://www.gnu.org/licenses/gpl.html GNU Public License v3 or later
+*/
+class LPC_HTTP_image extends LPC_HTTP_base
+{
+	/**
+	* The GD image resource.
+	*/
+	var $imgRes=NULL;
+
+	function ensureRes()
+	{
+		if ($this->imgRes)
+			return;
+		$this->imgRes=imagecreatefromstring($this->content);
+	}
+
+	function showJPEG($quality=75)
+	{
+		$this->prepareJPEG($quality);
+		$this->show();
+	}
+
+	function prepareJPEG($quality=75)
+	{
+		$this->ensureRes();
+		$this->mimeType="image/jpeg";
+		ob_start();
+		imagejpeg($this->imgRes,NULL,$quality);
+		$this->content=ob_get_clean();
+	}
+
+	function showPNG()
+	{
+		$this->preparePNG();
+		$this->show();
+	}
+
+	function preparePNG()
+	{
+		$this->ensureRes();
+		$this->mimeType="image/png";
+		ob_start();
+		imagepng($this->imgRes);
+		$this->content=ob_get_clean();
+	}
+
+	/**
+	* Resizes the image proportionally as to fit it into a box of the specified size.
+	*/
+	function fitToBox($maxWidth,$maxHeight=0)
+	{
+		$this->ensureRes();
+		if (!$maxHeight)
+			$maxHeight=$maxWidth;
+
+		$width=$orig_width=imagesx($this->imgRes);
+		$height=$orig_height=imagesy($this->imgRes);
+
+		list($width,$height)=$this->computeResize($width,$height,$width,$maxWidth);
+		list($width,$height)=$this->computeResize($width,$height,$height,$maxHeight);
+
+		if ($width==$orig_width && $height==$orig_height)
+			return;
+
+		$dst=imagecreatetruecolor($width,$height);
+		imagealphablending($dst,false);
+		imagesavealpha($dst, true);
+		imagecopyresampled($dst,$this->imgRes,0,0,0,0,$width,$height,$orig_width,$orig_height);
+
+		$this->imgRes=$dst;
+	}
+
+	function computeResize($width,$height,$metric,$max)
+	{
+		$factor=1;
+
+		if ($metric>$max)
+			$factor=$max/$metric;
+
+		$width*=$factor;
+		$height*=$factor;
+
+		return array($width,$height);
+	}
+}
