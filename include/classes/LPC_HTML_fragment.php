a1460ac9ae38de711a723034bf4451f3fdc5bcad
Bogdan Stancescu <bogdan@moongate.ro>
2012-02-13 01:03:25 +0200

    Sometimes we actually do want to indent...

--- include/classes/LPC_HTML_fragment.php
+++ include/classes/LPC_HTML_fragment.php
@@ -2,10 +2,12 @@
 
 class LPC_HTML_fragment extends LPC_HTML_base
 {
+	public $allowIndent=false;
 
 	function render()
 	{
-		$this->indentCount--;
+		if (!$this->allowIndent)
+			$this->indentCount--;
 		parent::render();
 		return $this->renderItem($this->content);
 	}
