b1abbdc81bd893c827c3e44fbf10e15848bc6078
Bogdan Stancescu <bogdan@moongate.ro>
2013-05-31 18:21:43 +0300

    Cleanup; constants; refactored show() to always call the parent's method

--- include/classes/LPC_HTML_document.php
+++ include/classes/LPC_HTML_document.php
@@ -14,10 +14,13 @@ class LPC_HTML_document extends LPC_HTML_node
 	public $title_default='LPC Project';
 	public $title_format='LPC &ndash; %s';
 
-	// 'HTML' for regular rendering
-	// 'none' for no rendering
-	// 'raw' to render only $body
-	public $renderMode='HTML';
+	// Render mode constants
+	const RM_NONE = "none"; // don't show anything
+	const RM_HTML = "HTML"; // full rendering (default)
+	const RM_RAW  = "raw";  // raw rendering (just the body)
+
+	// One of the above
+	public $renderMode=self::RM_HTML;
 
 	public $gz_enable=true; // disable if you're processing it yourself
 	public $gz_thresh=10240; // 10 KiB
@@ -41,18 +44,15 @@ class LPC_HTML_document extends LPC_HTML_node
 		$this->content['head']->a($title,'title');
 	}
 
-	public function show()
+	public function show($once = false)
 	{
-		if (!$this->gz_enable || $this->renderMode=='none')
-			return parent::show();
+		ob_start();
+		parent::show($once);
+		$output = ob_get_clean();
 
-		$output=$this->render();
-		if (strlen($output)<$this->gz_thresh) {
-			echo $output;
-			return;
-		}
+		if ($this->gz_enable && strlen($output) > $this->gz_thresh)
+			ob_start("ob_gzhandler");
 
-		ob_start("ob_gzhandler");
 		echo $output;
 	}
 
@@ -61,14 +61,18 @@ class LPC_HTML_document extends LPC_HTML_node
 		$this->prepareContent($this->content);
 		if ($this->doctype & LPC_HTML_doctype::type_XHTML1)
 			$this->setAttr("xmlns","http://www.w3.org/1999/xhtml");
-		if (!$this->body->content || $this->renderMode=='none')
+
+		if (!$this->body->content || self::RM_NONE == $this->renderMode)
 			return '';
 
 		if (!$this->beforeRender($indent))
 			return '';
 
-		if ($this->renderMode=='raw')
+		if (self::RM_RAW == $this->renderMode)
 			return $this->body->renderContent($indent);
+
+		if (self::RM_HTML != $this->renderMode)
+			throw new RuntimeException("Unknown render mode: [".$this->renderMode."]");
 
 		$title="";
 		if ($this->title) {
@@ -85,7 +89,6 @@ class LPC_HTML_document extends LPC_HTML_node
 
 		$content=$this->output(LPC_HTML_doctype::$doctypes[$this->doctype],$indent); // <!DOCTYPE...>
 		$content.=parent::render($indent);
-//mail("bogdan@moongate.ro","render",print_r($this->content['head'],1));
 		$this->onRender($indent);
 		return $content;
 	}
