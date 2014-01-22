3d14a602fa4b5b467e148a7aa9b3850d92c9003c
Bogdan Stancescu <gutza@moongate.ro>
2014-01-22 14:33:50 +0200

    Cosmetic (code)

--- include/classes/LPC_HTML_script.php
+++ include/classes/LPC_HTML_script.php
@@ -5,7 +5,7 @@ class LPC_HTML_script extends LPC_HTML_node
 	public $nodeName="script";
 	public $shortTag=false;
 
-	public function __construct($src=NULL,$type="text/javascript")
+	public function __construct($src=NULL, $type="text/javascript")
 	{
 		if (isset($src))
 			$this->setAttr('src',$src);
